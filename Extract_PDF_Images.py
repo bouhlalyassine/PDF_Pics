bba71224d1d2b4fce5f865788f7ebe9158baee24
Bouhlal Yassine <bouhlalyas@gmail.com>
2023-09-19 20:12:49 +0100

    inital commit

---
+++ Extract_PDF_Images.py
@@ -0,0 +1,123 @@
+import streamlit as st
+from settings import *
+from streamlit_lottie import st_lottie
+import streamlit as st
+from io import BytesIO
+
+
+# streamlit run Extract_PDF_Images.py
+
+st.set_page_config(page_title=TITLE,
+    page_icon=PAGE_ICON,
+    layout="wide")
+
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f: # Load the CSS file
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+
+st.markdown("<h2 style=\
+        'text-align : center';\
+        font-weight : bold ;\
+        font-family : Arial;>\
+        Extract Images from PDF</h2>", unsafe_allow_html=True)
+st.markdown("---")
+
+with st.sidebar :
+    clickable_img_logo = get_img_with_href(pp_logo_portfolio, 'https://ybouhlal.streamlit.app/', 70, "blank")
+    st.markdown(clickable_img_logo, unsafe_allow_html=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    clickable_img = get_img_with_href(linkpic_code, 'https://github.com/bouhlalyassine/PDF_Pics',
+        170, "blank")
+    st.markdown(clickable_img, unsafe_allow_html=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    uploaded_file = st.file_uploader("📌 Upload PDF", type=["pdf"], key="fup1", label_visibility="visible")
+
+
+if uploaded_file :          
+    images = extract_img(uploaded_file)
+    namefile = uploaded_file.name
+    img_idx = 0
+    all_images = []
+    
+    with st.spinner("Extraction in progress, please wait..."):
+        for image in images:
+            all_images.append(image)
+
+    if len(images)==0:
+        st.error("No image detected")
+
+    else :
+        colpi1, colpi2, colpi3 = st.columns([41, 18, 41], gap="small")
+
+        with colpi2:
+            st.markdown(f"<h5 style=\
+            'text-align : left';\
+            font-weight : bold ;\
+            font-family : Arial;>\
+            <b>{len(all_images)} Images detected</h5></b>", unsafe_allow_html=True)
+
+            img_idx = st.number_input(label="Label", value=1, min_value=1, max_value=len(all_images),
+                step=1, label_visibility="collapsed")
+
+        with colpi3:
+            zip_file_name = f"Images_{uploaded_file.name[:-4]}.zip" 
+            zip_file = generate_zip_file(all_images)
+
+            st.download_button(
+                label = "Download All images",
+                data = zip_file,
+                file_name = zip_file_name,
+                mime = "application/zip")
+
+        img_displ = all_images[img_idx - 1]
+        
+        with colpi1:
+            try :
+                buf = BytesIO()
+                img_displ.save(buf, format="JPEG")
+                byte_im = buf.getvalue()
+            except:
+                buf = BytesIO()
+                img_displ.save(buf, format="PNG")
+                byte_im = buf.getvalue()
+
+            st.download_button(
+                label="Download this image",
+                data=byte_im,
+                file_name=f"image_{img_idx}.png",
+                mime="image")
+        
+        # img = disp_chart_img(img_displ)
+        # img_chart= st.plotly_chart(img, use_container_width=True)
+        img_width = resize_image(img_displ)
+        img_chart = st.image(image = img_displ, width = img_width) # use_column_width=True
+
+else:
+    st.markdown("<br>", unsafe_allow_html=True)
+    colpi_1, colpi_2 = st.columns([85, 15], gap="small")
+    with colpi_1 :
+        st.info("This tool allows you to extract images from a PDF : \
+            \n ● View them individually\
+            \n ● Download them, while keeping their original quality\
+            \n\n You can upload the PDF to be processed on the left menu")
+
+    with colpi_2:
+        lottie_pdfimg = load_lottiefile(lottie_pdf_img)
+        st_lottie(
+        lottie_pdfimg,
+        speed=1,
+        reverse=False,
+        loop=True,
+        quality="high", # medium ; high ; low
+        height=150)
+
+    esp_1, col_vid_tuto, esp_2 = st.columns([space, tuto_space, space], gap="small")
+    with col_vid_tuto :
+        with open(tuto_extr_img, "rb") as tuto_file:
+            tuto_extr_img_byte = tuto_file.read()
+        st.video(tuto_extr_img_byte)
---
+++ settings.py
@@ -0,0 +1,112 @@
+import json
+from pathlib import Path
+from PIL import Image
+import requests
+import io
+from pikepdf import Pdf, PdfImage
+import plotly.express as px
+import zipfile
+import numpy as np
+import plotly.express as px
+import streamlit as st
+import os
+import base64
+
+
+TITLE = "PDF Pics"
+PAGE_ICON ="ico_potfolio.ico"
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+
+
+# CSS
+css_file = current_dir / "main.css"
+
+# My Tutos :
+# size :
+space = 15
+tuto_space = 70
+
+tuto_extr_img_p = current_dir / "files" / "tuto_ext_img_pdf.mp4"
+tuto_extr_img = str(tuto_extr_img_p)
+
+pp_logo_portfolio = current_dir / "files" /  "logo_portfolio.png"
+linkpic_code = current_dir / "files" /  "code.png"
+
+lottie_pdf_img = current_dir / "files" / "pdf_img.json"
+
+
+def load_lottiefile(filepath : str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Clickable img
+@st.cache_data
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+@st.cache_data
+def get_img_with_href(local_img_path, target_url, width, loc):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}" target="_{loc}" style="display: flex; justify-content: center; align-items: center;">
+            <img src="data:image/{img_format};base64,{bin_str}" width="{width}" class="img-hover-effect">
+        </a>'''
+    return html_code
+
+
+def extract_img(pdf):
+    images = []
+    doc_pdf = Pdf.open(pdf)
+    for i, page in enumerate(doc_pdf.pages):
+        for j, (name, raw_image) in enumerate(page.images.items()):
+            image = PdfImage(raw_image)
+            images.append(image.as_pil_image())
+    return images
+
+def resize_image(img):
+    w = int(img.size[0])
+    if w > 500 :
+        width = 500
+    else :
+        width= w
+    return width
+
+def disp_chart_img(img_displ):
+    img_displ = np.array(img_displ)
+    img_displ = np.array(Image.fromarray(img_displ).convert('RGB'))
+
+    img = px.imshow(img_displ)
+    img.update_layout(
+    # height= 480,
+    xaxis_showgrid=False,
+    yaxis_showgrid=False,
+    xaxis_visible=False,
+    yaxis_visible=False)
+
+    return img
+
+def generate_zip_file(imgz):
+    buffer = io.BytesIO()
+    with zipfile.ZipFile(buffer, 'w') as zip_file:
+        for i, img in enumerate(imgz):
+            img_buffer = io.BytesIO()
+            try :
+                img.save(img_buffer, format="JPEG")
+            except :
+                img.save(img_buffer, format="PNG")
+            img_buffer.seek(0)
+            zip_file.writestr(f"img_{i+1}.png", img_buffer.read())
+    buffer.seek(0)
+    return buffer.read()
+
